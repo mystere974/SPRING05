292bc8c6450927b8ef4f0ba0989eb7f4bb6158f3
loghann <m.loghann@gmail.com>
2022-11-08 16:51:31 +0100

    FIRST COMMIT

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -1,9 +1,6 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
@@ -18,6 +15,10 @@ public class Wizard {
     private String birthPlace;
     private String biography;
     private boolean muggle;
+
+    @ManyToOne
+    @JoinColumn(name = "school_id")
+    private School school;
 
     public Wizard() {
     }
@@ -72,6 +73,14 @@ public class Wizard {
 
     public boolean isMuggle() {
         return muggle;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
     }
 
     public void setMuggle(boolean muggle) {
